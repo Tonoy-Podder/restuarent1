db9d8beb67a553cc60d491a5b4b259802db41cd3
unknown <Tonoy>
2022-05-10 16:04:32 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoList';
+  list:any[]=[];
+  addTask(iteam:string)
+  {
+     this.list.push({id:this.list.length,name:iteam})
+     console.warn(this.list);
+
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(iteam=>iteam.id!==id);
+  }
+  getValue()
+  {
+    return "Tonoy Podder"
+  }
+  getValue1()
+  {
+    return "Created By"
+  }
+
 }
